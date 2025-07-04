cd89d42914bc92f89c950a4432b53434c84631a5
Rory Eddleston <rory_94@live.co.uk>
2025-07-04 12:36:31 +0100

    added type safety on new api keys

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,13 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_FIREBASE_API_KEY: string;
-    readonly VITE_FIREBASE_AUTH_DOMAIN: string;
-    readonly VITE_FIREBASE_PROJECT_ID: string;
-  }
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_UNSPLASH_ACCESS_KEY: string;
+  readonly VITE_OPENWEATHER_API_KEY: string;
+}
 
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
